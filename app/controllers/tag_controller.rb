41d575d592f44083488acaa1392eb1dd164d5ce7
clonex10100 <clonex10100@gmail.com>
2019-11-13 17:04:36 -0500

    Added more tag routes

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,11 +1,46 @@
 class TagController < ApplicationController
   get '/tags/new' do
+    unless Helpers.is_logged_in?(session)
+      flash[:message] = "You must be logged in to access this page"
+      redirect "/login"
+    end
     @shows = Helpers.current_user(session).shows
     erb :'tags/new'
   end
 
   post '/tags' do
     @tag = Tag.create(params[:tag])
+    @tag.user = Helpers.current_user(session)
     redirect '/'
+  end
+
+  get '/tags' do
+    unless Helpers.is_logged_in?(session)
+      flash[:message] = "You must be logged in to access this page"
+      redirect "/login"
+    end
+    @tags = Helpers.current_user(session).tags
+    erb :'tags/show'
+  end
+
+  get '/tags/:id/edit' do
+    @tag = Tag.find(params[:id])
+    if @tag.user.id == session[:id]
+      @shows = Helpers.current_user(session).shows
+      erb :'tags/edit'
+    else
+      flash[:message] = "You must be logged in to access this page"
+      redirect "/login"
+    end
+  end
+
+  patch '/tags/:id' do
+    @tag = Tag.find(params[:id])
+    @tag.update(params[:tag])
+    if !params[:tag][:show_ids]
+      @tag.shows.clear
+      @tag.save
+    end
+    redirect '/tags'
   end
 end
